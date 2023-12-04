05cf3ff02e273f50837eb2420508eda93c778e4c
RoKaTG <msiliniyassine@gmail.com>
2023-12-04 22:23:17 +0100

    Updating the parser to handle redis format

--- parser.c
+++ parser.c
@@ -7,23 +7,22 @@
 
 Command parse_command(const char *input) {
     Command cmd;
-    char command[256];
-
-    if (sscanf(input, "%s %s %s", command, cmd.key, cmd.value) < 1) {
-        cmd.type = CMD_UNKNOWN;
-        return cmd;
-    }
-
-    if (strcmp(command, "PING") == 0) {
-        cmd.type = CMD_PING;
-    } else if (strcmp(command, "SET") == 0) {
-        cmd.type = CMD_SET;
-    } else if (strcmp(command, "GET") == 0) {
-        cmd.type = CMD_GET;
-    } else if (strcmp(command, "DEL") == 0) {
-        cmd.type = CMD_DEL;
-    } else {
-        cmd.type = CMD_UNKNOWN;
+    memset(&cmd, 0, sizeof(Command));
+    char *line = strtok(input, "\r\n");
+    while (line != NULL) {
+        if (line[0] == '*') {
+        } else if (line[0] == '$') {
+        } else {
+            if (cmd.type == CMD_UNKNOWN) {
+                if (strcmp(line, "PING") == 0) {
+                    cmd.type = CMD_PING;
+                }
+                //
+            } else {
+                strncpy(cmd.argument, line, sizeof(cmd.argument) - 1);
+            }
+        }
+        line = strtok(NULL, "\r\n");
     }
 
     return cmd;
