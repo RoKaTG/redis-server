7e67d355daa2bd28523204c5d83625c342e34f8d
RoKaTG <msiliniyassine@gmail.com>
2023-12-04 23:04:38 +0100

    Added hashmap structure & handler

---
+++ hashmap.c
@@ -0,0 +1,73 @@
+#include "hashmap.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define HASHMAP_SIZE 1024
+
+unsigned int hash(const char *key) {
+    unsigned long int value = 0;
+    unsigned int i = 0;
+    unsigned int key_len = strlen(key);
+
+    for (; i < key_len; ++i) {
+        value = value * 37 + key[i];
+    }
+    return value % HASHMAP_SIZE;
+}
+
+hashmap* hashmap_create() {
+    hashmap *h = malloc(sizeof(hashmap));
+    h->entries = malloc(sizeof(hashmap_entry*) * HASHMAP_SIZE);
+
+    for (int i = 0; i < HASHMAP_SIZE; ++i) {
+        h->entries[i] = NULL;
+    }
+
+    return h;
+}
+
+void hashmap_set(hashmap *h, const char *key, const char *value) {
+    unsigned int slot = hash(key);
+    hashmap_entry *entry = h->entries[slot];
+
+    while (entry != NULL) {
+        if (strcmp(entry->key, key) == 0) {
+            strncpy(entry->value, value, sizeof(entry->value));
+            return;
+        }
+        entry = entry->next;
+    }
+
+    entry = malloc(sizeof(hashmap_entry));
+    strncpy(entry->key, key, sizeof(entry->key));
+    strncpy(entry->value, value, sizeof(entry->value));
+    entry->next = h->entries[slot];
+    h->entries[slot] = entry;
+}
+
+char* hashmap_get(hashmap *h, const char *key) {
+    unsigned int slot = hash(key);
+    hashmap_entry *entry = h->entries[slot];
+
+    while (entry != NULL) {
+        if (strcmp(entry->key, key) == 0) {
+            return entry->value;
+        }
+        entry = entry->next;
+    }
+    return NULL;
+}
+
+void hashmap_free(hashmap *h) {
+    for (int i = 0; i < HASHMAP_SIZE; ++i) {
+        hashmap_entry *entry = h->entries[i];
+        while (entry != NULL) {
+            hashmap_entry *temp = entry;
+            entry = entry->next;
+            free(temp);
+        }
+    }
+    free(h->entries);
+    free(h);
+}
+
---
+++ hashmap.h
@@ -0,0 +1,22 @@
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+typedef struct hashmap_entry {
+    char key[256];
+    char value[256];
+    struct hashmap_entry *next;
+} hashmap_entry;
+
+
+typedef struct {
+    hashmap_entry **entries;
+} hashmap;
+
+
+hashmap* hashmap_create();
+void hashmap_set(hashmap *h, const char *key, const char *value);
+char* hashmap_get(hashmap *h, const char *key);
+void hashmap_free(hashmap *h);
+
+#endif // HASHMAP_H
+
