0117fcf51cbc54e4823da03c948fe953d9dcf2ad
RoKaTG <msiliniyassine@gmail.com>
2023-12-05 00:39:10 +0100

    Added the support of GET in the parser

--- parser.c
+++ parser.c
@@ -9,7 +9,7 @@ Command parse_command(const char *input) {
 
     char *line = strtok(input, "\r\n");
     int line_count = 0;
-    
+
     while (line != NULL) {
         if (line[0] == '*') {
         } else if (line[0] == '$') {
@@ -20,9 +20,13 @@ Command parse_command(const char *input) {
                     cmd.type = CMD_PING;
                 } else if (strcmp(line, "SET") == 0) {
                     cmd.type = CMD_SET;
+                } else if (strcmp(line, "GET") == 0) {
+                    cmd.type = CMD_GET;
                 }
             } else if (line_count == 2) {
                 if (cmd.type == CMD_SET) {
+                    strncpy(cmd.key, line, sizeof(cmd.key) - 1);
+                } else if (cmd.type == CMD_GET) {
                     strncpy(cmd.key, line, sizeof(cmd.key) - 1);
                 } else {
                     strncpy(cmd.argument, line, sizeof(cmd.argument) - 1);
@@ -32,9 +36,6 @@ Command parse_command(const char *input) {
             }
         }
         line = strtok(NULL, "\r\n");
-    }
-    if (cmd.type == CMD_PING && line_count == 1) {
-        memset(cmd.argument, 0, sizeof(cmd.argument));
     }
 
     return cmd;
